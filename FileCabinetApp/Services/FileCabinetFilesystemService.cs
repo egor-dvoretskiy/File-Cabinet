2d67d4a6f2bfd46ca13d26c7805bd4965065f596
Egor Dvoretskiy <brainispower21@gmail.com>
2021-12-11 00:25:23 +0300

    Implement CreateRecord for FileCabinetFilesystemService

--- FileCabinetApp/Services/FileCabinetFilesystemService.cs
+++ FileCabinetApp/Services/FileCabinetFilesystemService.cs
@@ -12,7 +12,15 @@ namespace FileCabinetApp
     /// </summary>
     public class FileCabinetFilesystemService : IFileCabinetService
     {
+        private const int RecordSize = 277; // bytes
+
         private FileStream fileStream;
+
+        private Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private Dictionary<string, List<FileCabinetRecord>> lastNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private Dictionary<string, List<FileCabinetRecord>> dateOfBirthDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+
+        private int recordsCount = 0;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FileCabinetFilesystemService"/> class.
@@ -26,7 +34,41 @@ namespace FileCabinetApp
         /// <inheritdoc/>
         public int CreateRecord(FileCabinetRecord record)
         {
-            throw new NotImplementedException();
+            try
+            {
+                record.Id = ++this.recordsCount;
+
+                using (BinaryWriter writer = new BinaryWriter(this.fileStream))
+                {
+                    writer.Seek(2, SeekOrigin.Begin);
+                    writer.Write(record.Id);
+
+                    writer.Write(record.FirstName.ToCharArray());
+                    writer.Seek(120 - record.FirstName.Length, SeekOrigin.Current);
+
+                    writer.Write(record.LastName.ToCharArray());
+                    writer.Seek(120 - record.LastName.Length, SeekOrigin.Current);
+
+                    writer.Write(record.DateOfBirth.Year);
+                    writer.Write(record.DateOfBirth.Month);
+                    writer.Write(record.DateOfBirth.Day);
+                    writer.Write(record.PersonalRating);
+                    writer.Write(record.Debt);
+                    writer.Write(record.Gender);
+                }
+
+                return record.Id;
+            }
+            catch (ArgumentNullException anex)
+            {
+                Console.WriteLine(anex.Message);
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+            }
+
+            return -1;
         }
 
         /// <inheritdoc/>
