5f30a3ac58bb21861dbab9f9d187affcf576b261
ergo lance <brainispower21@gmail.com>
2022-02-15 14:54:08 +0300

    Add skeleton for MongoService.

---
+++ FileCabinetApp/Services/FileCabinetMongoService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileCabinetApp.Interfaces;
+using FileCabinetApp.ServiceTools;
+
+namespace FileCabinetApp.Services
+{
+    /// <summary>
+    /// Class for work with noSQL mongoDB.
+    /// </summary>
+    internal class FileCabinetMongoService : IFileCabinetService
+    {
+        /// <inheritdoc/>
+        public bool CheckRecordPresence(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void CreateRecord(FileCabinetRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Delete(List<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindByBirthDate(string birthDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindByFirstName(string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindByGender(string gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindByPersonalRating(string personalRating)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<FileCabinetRecord> FindBySalary(string salary)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public FileCabinetRecord GetRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void GetStat()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void InsertRecord(FileCabinetRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public FileCabinetServiceSnapshot MakeSnapshot()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Purge()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Restore(FileCabinetServiceSnapshot fileCabinetServiceSnapshot)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public List<FileCabinetRecord> Select(string phrase, string memoizingKey, IRecordInputValidator inputValidator)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Update(List<FileCabinetRecord> records)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
